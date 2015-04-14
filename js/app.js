6d2e451135f67971459a75bf5d746e4c1921bc13
Artem Demo <artemdemo@gmail.com>
2015-04-14 23:49:06 +0300

    Added base configuration

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 /// <reference path="types/snapsvg.d.ts" />
+/**
+ * Base Class for control hexagon
+ */
 var Base = (function () {
     function Base(gameID) {
         var wHeight;
@@ -7,13 +10,42 @@ var Base = (function () {
         // calculate height of the paper
         wHeight = window.innerHeight - 10;
         this.$gamePaper.node.style.height = wHeight + '.px';
+        // Setting up basic data about control hexagon element
+        this.$base.baseX = window.innerWidth / 2;
+        this.$base.baseY = window.innerHeight / 2;
+        this.$base.radius = 150;
         this.drawBase();
     }
     /**
      * Drawing the main control element of the game
      */
     Base.prototype.drawBase = function () {
-        this.$base = this.$gamePaper.circle(150, 150, 100);
+        var startAngle = 0, endAngle = 0; // startAngle & endAngle are in degrees
+        var baseRadius = this.$base.radius;
+        var edgesNum = 6;
+        var x1, x2, y1, y2 = 0;
+        var startX, startY;
+        for (var i = 0; i < edgesNum; i++) {
+            startAngle = endAngle;
+            endAngle = startAngle + 30;
+            x1 = this.$base.baseX + baseRadius * Math.cos(Math.PI * startAngle / 180);
+            y1 = this.$base.baseY + baseRadius * Math.sin(Math.PI * startAngle / 180);
+            x2 = this.$base.baseX + baseRadius * Math.cos(Math.PI * endAngle / 180);
+            y2 = this.$base.baseY + baseRadius * Math.sin(Math.PI * endAngle / 180);
+            /*
+             * Solving problem of not fitting the last sector with the first one
+             * This problem cases by number rounding, and the easiest way to solve it - is to use the same coordinates for the last point as for the first one
+             */
+            if (i == 0) {
+                startX = x1;
+                startY = y1;
+            }
+            else if (i == edgesNum - 1) {
+                x2 = startX;
+                y2 = startY;
+            }
+        }
+        this.$base.baseEl = this.$gamePaper.circle(150, 150, 100);
     };
     return Base;
 })();
--- source/Base_class.ts
+++ source/Base_class.ts
@@ -1,5 +1,18 @@
 /// <reference path="types/snapsvg.d.ts" />
 
+/**
+ * Interface for basic configuration of control hexagon
+ */
+interface baseDataConfig {
+    baseEl: any;
+    baseX: number;
+    baseY: number;
+    radius: number;
+}
+
+/**
+ * Base Class for control hexagon
+ */
 class Base {
     /**
      * Main game paper
@@ -9,7 +22,7 @@ class Base {
     /**
      * Base control element of the game
      */
-    $base;
+    $base: baseDataConfig;
 
     constructor(gameID: string) {
         var wHeight:number;
@@ -21,6 +34,10 @@ class Base {
         wHeight = window.innerHeight - 10;
         this.$gamePaper.node.style.height = wHeight + '.px';
 
+        // Setting up basic data about control hexagon element
+        this.$base.baseX = window.innerWidth / 2;
+        this.$base.baseY = window.innerHeight / 2;
+        this.$base.radius = 150;
 
         this.drawBase();
     }
@@ -29,7 +46,34 @@ class Base {
      * Drawing the main control element of the game
      */
     drawBase() {
-        this.$base = this.$gamePaper.circle(150, 150, 100);
+        var startAngle = 0, endAngle = 0; // startAngle & endAngle are in degrees
+        var baseRadius = this.$base.radius;
+        var edgesNum = 6;
+        var x1,x2,y1,y2 = 0;
+        var startX, startY;
+
+        for ( var i=0; i<edgesNum; i++ ) {
+            startAngle = endAngle;
+            endAngle = startAngle + 30;
+
+            x1 = this.$base.baseX + baseRadius*Math.cos(Math.PI*startAngle/180);
+            y1 = this.$base.baseY + baseRadius*Math.sin(Math.PI*startAngle/180);
+
+            x2 = this.$base.baseX + baseRadius*Math.cos(Math.PI*endAngle/180);
+            y2 = this.$base.baseY + baseRadius*Math.sin(Math.PI*endAngle/180);
+
+            /*
+             * Solving problem of not fitting the last sector with the first one
+             * This problem cases by number rounding, and the easiest way to solve it - is to use the same coordinates for the last point as for the first one
+             */
+            if ( i == 0 ) {
+                startX = x1; startY = y1;
+            } else if ( i == edgesNum - 1 ) {
+                x2 = startX; y2 = startY;
+            }
+        }
+
+        this.$base.baseEl = this.$gamePaper.circle(150, 150, 100);
     }
 
 }
